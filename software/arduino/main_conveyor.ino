59cd7701897beab1fec71546b243b80053e1be18
Alakey <Salamakhin67@gmail.com>
2022-03-09 19:46:20 +0300

    init commit

---
+++ software/arduino/main_conveyor.ino
@@ -0,0 +1,242 @@
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+#include "HX711.h"
+#include <L298N.h>
+HX711 scale;
+
+#include <Servo.h>
+Servo servo1;
+Servo servo2;
+const int pin_servo1 = 9;
+const int pin_servo2 = 10;
+const int angle_max = 180;
+//int angle = 0; //переменная, хранящая значение угла поворота
+//int dir = 1; //направление вращения
+int count1 = 0;
+int count2 = 0;
+int count3 = 0;
+int pos = 0;
+
+#define PIN_TRIG1 12
+#define PIN_ECHO1 11
+#define PIN_TRIG2 5
+#define PIN_ECHO2 4
+uint8_t dataPin = 2;
+uint8_t clockPin = 3;
+const unsigned int IN1 = 7;
+const unsigned int IN2 = 8;
+const unsigned int EN = 6;
+
+L298N motor(EN, IN1, IN2);
+
+float value;
+float speedneed;
+
+int w1, w2;
+long stoptime;
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+float TimeOfImpulse, DistanceToObject;
+int imptime, stopdistance;
+
+int R = 0;
+int G = 0;
+int B = 0;
+
+void setup(void) {
+
+  Serial.begin(9600);
+
+  pinMode(PIN_TRIG1, OUTPUT); //
+  pinMode(PIN_ECHO1, INPUT);
+  pinMode(PIN_TRIG2, OUTPUT);
+  pinMode(PIN_ECHO2, INPUT);
+  scale.begin(dataPin, clockPin);
+  scale.set_scale(-1100.0983);
+  scale.tare();
+
+  servo1.attach(pin_servo1);
+  servo2.attach(pin_servo2);
+}
+
+void loop() {
+
+  if (value == 1) {
+    motor.forward();
+    motor.setSpeed(255);
+    value = Serial.read();
+  }
+  if (value == 3) {
+    if (count1 < 1) {
+      for (pos = 180; pos >= 0; pos -= 1) {
+        servo1.write(pos);
+        servo2.write(pos);
+        delay(15);
+      }
+      for (pos = 0; pos <= 30; pos += 1) {
+        servo1.write(pos);
+        delay(15);
+      }
+      for (pos = 0; pos <= 5; pos += 1) {
+        servo2.write(pos);
+        delay(15);
+      }
+      count1 = count1 + 1;
+    }
+    else {
+      count1 = 0;
+      motor.forward();
+      motor.setSpeed(255);
+    }
+  }
+
+  if (value == 4) {
+    if (count2 < 1) {
+      for (pos = 180; pos >= 0; pos -= 1) {
+        servo2.write(pos);
+        servo1.write(pos);
+        delay(15);
+      }
+      for (pos = 0; pos <= 120; pos += 1) {
+        servo2.write(pos);
+        delay(15);
+      }
+      for (pos = 0; pos <= 5; pos += 1) {
+        servo1.write(pos);
+        delay(15);
+      }
+      count2 = count2 + 1;
+    }
+    else {
+      count2 = 0;
+      motor.forward();
+      motor.setSpeed(255);
+    }
+  }
+
+  if (value == 5) {
+    if (count3 < 1) {
+      for (pos = 180; pos >= 0; pos -= 1) {
+        servo1.write(pos);
+        servo1.write(pos);
+        delay(15);
+      }
+      for (pos = 0; pos <= 5; pos += 1) {
+        servo1.write(pos);
+        servo1.write(pos);
+        delay(15);
+      }
+      count3 = count3 + 1;
+    }
+    else {
+      count3 = 0;
+      motor.forward();
+      motor.setSpeed(255);
+    }
+  }
+
+  if ((value != 1) && (value != 0) && (value != 2) && (value != 3) && (value != 4) && (value != 5)) {
+    motor.forward();
+    motor.setSpeed(value);
+    value = Serial.read();
+  }
+
+  if (value == 2) {
+    motor.stop();
+    value = Serial.read();
+  }
+
+
+  uint16_t red, green, blue, clear;
+  tcs.getRawData(&red, &green, &blue, &clear);
+
+  double sum = clear;
+  double r, g, b;
+
+  r = red;
+  r /= sum;
+
+  g = green;
+  g /= sum;
+
+  b = blue;
+  b /= sum;
+
+  r *= 256;
+  g *= 256;
+  b *= 256;
+
+  R = r + 100;
+  G = g + 100;
+  B = b + 100;
+
+  w1 = scale.get_units(10);
+  w2 = scale.get_units();
+  while (abs(w1 - w2) > 10)
+  {
+    w1 = w2;
+    w2 = scale.get_units();
+  }
+  if (w1 < 0.5)
+  {
+    Serial.print(100);
+    Serial.print(" ");
+  }
+  else
+  {
+    w1 = w1 + 100;
+    Serial.print(w1);
+    Serial.print(" ");
+  }
+
+  digitalWrite(PIN_TRIG1, LOW);
+  delayMicroseconds(5);
+  digitalWrite(PIN_TRIG1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG1, LOW);
+
+  TimeOfImpulse = pulseIn(PIN_ECHO1, HIGH);
+  DistanceToObject = TimeOfImpulse * 0.034 / 2;
+
+  if (DistanceToObject <= 10)
+  {
+    Serial.print(1);
+    Serial.print(" ");
+    Serial.print(DistanceToObject);
+  }
+  else
+  {
+    Serial.print(0);
+    Serial.print(" ");
+    Serial.print(0.00);
+  }
+
+  Serial.print(" "); Serial.print(R, DEC); Serial.print(" "); Serial.print(G, DEC);  Serial.print(" "); Serial.print(B, DEC); Serial.print(" ");
+
+  if (Serial.available() > 0)
+  {
+    value = Serial.read();
+  }
+
+  digitalWrite(PIN_TRIG2, LOW);
+  delayMicroseconds(5);
+  digitalWrite(PIN_TRIG2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG2, LOW);
+
+  imptime = pulseIn(PIN_ECHO2, HIGH);
+  stopdistance = imptime * 0.034 / 2;
+
+  if ((stopdistance >= 35) || (stopdistance <= 10))
+  {
+    Serial.print(99);
+    Serial.println(" ");
+  }
+  else
+  {
+    Serial.print(stopdistance);
+    Serial.println(" ");
+  }
+
+}
---
+++ software/matlab/m_conveyor.m
@@ -0,0 +1,240 @@
+clc
+close all
+clear all
+
+global s
+global needspeed
+global warningpress
+
+delete(instrfind)
+s = serialport('COM4', 9600);
+
+% speed = inputdlg({'Input conveyor speed'},'Ввод данных', [1 50]);
+% V = str2num(cell2mat(speed));
+V = 3;
+
+% colourlist = {'Red','Yellow','Light blue','Blue','Green','Purple','Pink'};
+% [indx,tf] = listdlg('ListString',colourlist);
+indx = [1, 2, 5, 6, 7];
+
+% ab = inputdlg({'Input min weight','Input max weight'},'Ввод данных', [1 50]);
+% WW = str2double(ab);
+WW = [0; 100];
+
+k=1; total=0; colour=0; coun=1; sumweight=0; start_measurment = 0; cnt=0;...
+    measurement_end2 = 0; needspeed=0; j=1; m=1; weightcnt=0; w=1;
+
+
+datatabl=zeros(8); %%все данные с USB-порта
+rgbtabl = zeros(3); %%массив для записи значений цвета в RGB в диапазоне 0-1
+hsvtabl = zeros(3); %%массив для записи значений цвета в HSV в диапазоне 0-1
+lenghttabl=zeros(2); %%массив для записи значений длины предмета
+weighttabl=zeros(1); %%массив для записи значений массы предмета
+usertabl = zeros(5);
+stopdistance = zeros(1);
+
+data="";
+
+fig = uifigure('Name','RESULT');
+fig.Position = [50   50   600   500];
+ppanel1 = uipanel(fig,'Position',[50 50 140 140]);
+lmp = uilamp(ppanel1,'Position',[20 20 100 100]);
+
+ppanel2 = uipanel(fig,'Position',[100 200 90 30]);
+txa1 = uitextarea(ppanel2,'Position',[0 0 90 30],'HorizontalAlignment', 'center');
+
+ppanel3 = uipanel(fig,'Position',[200 200 90 30]);
+txa2 = uitextarea(ppanel3,'Position',[0 0 90 30],'HorizontalAlignment', 'center');
+
+f=figure;
+sliderdata=uicontrol(f,'BackgroundColor','#0072BD','style','Slider','Min',0,'Max',1,...
+    'Value',1,'units','normalized');
+
+sliderdata.Position = [0.1 0.1 0.05 0.8];
+
+pan1 = uipanel(f,'Position',[0.17 0.2 0.2 0.1]);
+btn1=uicontrol(pan1,'BackgroundColor',	'red', 'style','pushbutton',...
+    'String','Stop','CallBack',@PushButton1, 'Position', [1.6 1.2 108 38.79 ]);
+
+pan2 = uipanel(f,'Position',[0.17 0.3 0.2 0.1]);
+btn2=uicontrol(pan2,'BackgroundColor',	'green','style','pushbutton',...
+    'String','Max speed','CallBack', @PushButton2, 'Position', [1.6 1.2 108 38.79 ]);
+
+pan3 = uipanel(f,'Position',[0.17 0.4 0.2 0.1]);
+btn3=uicontrol(pan3,'style','pushbutton', 'String','Set speed',...
+    'CallBack', {@PushButton3, sliderdata, needspeed}, 'Position', [1.6 1.2 108 38.79 ]);
+
+pan4 = uipanel(f,'Position',[0.18 0.6 0.3 0.1]);
+btn4=uicontrol(pan4,'style','pushbutton', 'String','Ручное управление',...
+    'CallBack', {@PushButton4, warningpress}, 'Position', [1.6 1.7 164 38.79 ]);
+
+pan5 = uipanel(f,'Position',[0.53 0.6 0.3 0.1]);
+bt5=uicontrol(pan5,'style','pushbutton', 'String','Автомат. управление',...
+    'CallBack', {@PushButton5, warningpress}, 'Position', [1.6 1.7 164 38.79 ]);
+
+while (true)
+    f=gcf;
+    
+    for i = 1:10000
+        for w = 1:10000
+            write(s, 1, "string");
+            data = read(s,28,"string");
+            datatabl(i,:)=double(split(data))';
+            
+            usertabl(w,1)=w;
+            
+            if warningpress == 1
+                write(s, needspeed, "string");
+            else
+                
+                stopdistance(i) = datatabl(i,7);
+                %масса предмета
+                
+                if ((stopdistance(i)>=15)&&(stopdistance(i)<=20)&& (start_measurment==0))
+                    start_measurment = 1;
+                end
+                if ((start_measurment == 1)&&(j<=3))
+                    write(s, 2, "string");
+                    
+                    weighttabl(j) = datatabl(i,1)-100;
+                    
+                    % перевод цвета из RGB в HSV
+                    
+                    rgbtabl(j, 1) = (datatabl(i,4)-100)./256; %%перевод в диапазон значений 0-1
+                    rgbtabl(j, 2)= (datatabl(i,5)-100)./256;
+                    rgbtabl(j, 3)= (datatabl(i,6)-100)./256;
+                    hsvtabl(j,:)= rgb2hsv(rgbtabl(j, :)); %%перевод из RGB в HSV
+                    
+                    usertabl(w, 2) = weighttabl(j);
+                    usertabl(w, 3) = hsvtabl(j,1);
+                    usertabl(w, 4) = hsvtabl(j,2);
+                    usertabl(w, 5) = hsvtabl(j,3);
+                    
+                    j=j+1;
+                    
+                else
+                    
+                    answer = sum(weighttabl)/3;
+                    weightanswer=sprintf(num2str(answer));
+                    txa2.Value=weightanswer;
+                    %%отображение цвета и его вывод
+                    
+                    if ((hsvtabl(i, 1)>0) && (hsvtabl(i, 1)<0.054))
+                        txa1.Value='red color';
+                        colour=1;
+                        lmp.Color = '#92000a';
+                    end
+                    if ((hsvtabl(i, 1)>0.054) && (hsvtabl(i, 1)<0.1265))
+                        txa1.Value='yellow color';
+                        colour=2;
+                        lmp.Color = '#ffd700';
+                    end
+                    if ((hsvtabl(i, 1)>0.1265) && (hsvtabl(i, 1)<0.3645))
+                        txa1.Value='green color';
+                        colour=5;
+                        lmp.Color = '#228b22';
+                    end
+                    if ((hsvtabl(i, 1)>0.3645) && (hsvtabl(i, 1)<0.486))
+                        txa1.Value='light blue color';
+                        colour=3;
+                        lmp.Color = '#6495ed';
+                    end
+                    if ((hsvtabl(i, 1)>0.486) && (hsvtabl(i, 1)<0.675))
+                        txa1.Value='blue color';
+                        colour=4;
+                        lmp.Color = '#310062';
+                    end
+                    if ((hsvtabl(i, 1)>0.675) && (hsvtabl(i, 1)<0.7425))
+                        txa1.Value='purple color';
+                        colour=6;
+                        lmp.Color = '#7E2F8E';
+                    end
+                    if ((hsvtabl(i, 1)>0.7425) && (hsvtabl(i, 1)<0.945))
+                        txa1.Value='pink color';
+                        colour=7;
+                        lmp.Color = '#ffc0cb';
+                    end
+                    
+                    colourfind=find(indx==colour);
+                    
+                    if ((answer<WW(1,1)) || (answer>WW(2,1)))
+                        write(s, 3, "string");
+                        %                     wrongcolour = warndlg('Объект не того веса ','Предупреждение ');
+                        %                     uiwait(wrongcolour,1)
+                        %                     close (wrongcolour)
+                        %                     weighttabl(j)=0;
+                        %                     m=m+1;
+                    end
+                    
+                    if isempty(colourfind)
+                        write(s, 4, "string");
+                    end
+                    
+                    if ((answer>WW(1,1)) && (answer<WW(2,1)))
+                        write(s, 5, "string");
+                    end 
+                    
+                    j=1;
+                    start_measurment=0;
+                end
+                
+                %                 lenghttabl(i,1) = datatabl(i,3);
+                %                 if (lenghttabl(i,1) ~=0)
+                %                     cnt=cnt+1;
+                %                     measurement_end2 = 1;
+                %                 else
+                %                     if measurement_end2 == 1
+                %                         averagelen = sum(lenghttabl ((i - cnt): length(lenghttabl))) / cnt;
+                %                         cathetus= averagelen.*0.42./0.906307;
+                %                         Objectlenght= cnt.*V-2.*cathetus;
+                %                         disp(['Final lenght: ', num2str(Objectlenght)])
+                %                     end
+                %                     measurement_end2 = 0;
+                %                     cnt =0;
+                %                     Objectlenght =0;
+                
+                
+                %%сообщение об ошибке, если цвет не соответсвует
+%                 colourfind=find(indx==colour);
+%                 if isempty(colourfind)
+%                     write(s, 4, "string")
+%                     %                     wrongcolour = warndlg('Объект не того цвета ','Предупреждение ');
+%                     %                     uiwait(wrongcolour,1)
+%                     %                     close (wrongcolour)
+%                 end
+
+            end
+            %             end
+        end
+        w = w+1;
+    end
+    i=i+1;
+end
+
+
+function PushButton1(src,~)
+global needspeed
+needspeed=2;
+end
+
+% start
+function PushButton2(src,~)
+global needspeed
+needspeed=1;
+end
+
+% set speed
+function PushButton3(src,~, sliderdata,~)
+global needspeed
+needspeed=sliderdata.Value.*255;
+end
+
+function PushButton4(src,~,~)
+global warningpress
+warningpress=1;
+end
+
+function PushButton5(src,~,~)
+global warningpress
+warningpress=0;
+end
